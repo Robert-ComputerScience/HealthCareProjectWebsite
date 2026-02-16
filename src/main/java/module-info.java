77e22a34df3774644eccebefd8c196ec9be95d3d
Robert-IT-CPU <shupr@farmingdale.edu>
2026-02-16 15:49:46 -0500

    Added Stuff Some Errors

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,7 @@ module com.example.healthcareprojectwebsite {
     requires com.google.auth;
     requires com.google.api.apicommon;
     requires google.cloud.core;
+    requires com.google.auth.oauth2;
 
     // Open the package to JavaFX for FXML and Firebase for Reflection
     opens com.example.healthcareprojectwebsite to javafx.fxml, google.cloud.firestore;
